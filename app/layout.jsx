26c6825c13acf7807a0e7e174b26a7ed831e4d26
Piotr Maciejewski <pether.maciejewski@gmail.com>
2023-11-30 15:12:49 +0100

    adding GA

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import { Outfit } from "next/font/google";
 import "./globals.css";
 
+import GoogleAnalytics from "@/components/GoogleAnalytics";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { ThemeProvider } from "@/components/ThemeProvider";
@@ -16,6 +17,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <GoogleAnalytics GA_MEASUREMENT_ID="G-1VYD1C0X5Q" />
       <body className={outfit.className}>
         <ThemeProvider attribute="class" defaultTheme="dark">
           <Header />
